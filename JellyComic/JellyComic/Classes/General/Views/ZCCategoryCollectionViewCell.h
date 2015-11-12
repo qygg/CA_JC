e3060c10168ef287bf6055f3a5dcc8d11726aaa0
胡智超 <1119364644@qq.com>
2015-11-12 19:52:28 +0800

    创建分类cell

---
+++ JellyComic/JellyComic/Classes/General/Views/ZCCategoryCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ZCCategoryCollectionViewCell.h
+//  JellyComic
+//
+//  Created by 胡智超 on 15/11/12.
+//  Copyright © 2015年 胡智超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZCCategoryCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ JellyComic/JellyComic/Classes/General/Views/ZCCategoryCollectionViewCell.m
@@ -0,0 +1,17 @@
+//
+//  ZCCategoryCollectionViewCell.m
+//  JellyComic
+//
+//  Created by 胡智超 on 15/11/12.
+//  Copyright © 2015年 胡智超. All rights reserved.
+//
+
+#import "ZCCategoryCollectionViewCell.h"
+
+@implementation ZCCategoryCollectionViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+@end
