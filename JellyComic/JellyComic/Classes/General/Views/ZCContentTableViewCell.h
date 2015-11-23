b161961de2f8b667397cb5b6d637ff5121956234
胡智超 <1119364644@qq.com>
2015-11-23 10:55:13 +0800

    新建cell

---
+++ JellyComic/JellyComic/Classes/General/Views/ZCContentTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ZCContentTableViewCell.h
+//  JellyComic
+//
+//  Created by 胡智超 on 15/11/22.
+//  Copyright © 2015年 胡智超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZCContentTableViewCell : UITableViewCell
+
+@end
---
+++ JellyComic/JellyComic/Classes/General/Views/ZCContentTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  ZCContentTableViewCell.m
+//  JellyComic
+//
+//  Created by 胡智超 on 15/11/22.
+//  Copyright © 2015年 胡智超. All rights reserved.
+//
+
+#import "ZCContentTableViewCell.h"
+
+@implementation ZCContentTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
