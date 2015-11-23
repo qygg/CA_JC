f635c24223d74876c926b5b6097aa7f7e1e073d0
Shishasha <1243534442@qq.com>
2015-11-23 11:54:50 +0800

    修改开始阅读Button

---
+++ JellyComic/JellyComic/Classes/Helpers/SSRollingBtn.h
@@ -0,0 +1,13 @@
+//
+//  SSRollingBtn.h
+//  JellyComic
+//
+//  Created by lanou3g on 15/11/23.
+//  Copyright © 2015年 胡智超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSRollingBtn : UIButton
+
+@end
---
+++ JellyComic/JellyComic/Classes/Helpers/SSRollingBtn.m
@@ -0,0 +1,21 @@
+//
+//  SSRollingBtn.m
+//  JellyComic
+//
+//  Created by lanou3g on 15/11/23.
+//  Copyright © 2015年 胡智超. All rights reserved.
+//
+
+#import "SSRollingBtn.h"
+
+@implementation SSRollingBtn
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
