21037f1a640fdda04dcfd9351a3c21caf49c941c
胡智超 <1119364644@qq.com>
2015-11-25 14:24:17 +0800

    修改为静态库

--- JellyComic/Pods/Headers/Public/AVOSCloudDynamic/AVOSCloud/AVAnonymousUtils.h
+++
@@ -1,1 +0,0 @@
-../../../../AVOSCloudDynamic/iOS/release-v3.1.6.3/Dynamic/AVOSCloud.framework/Headers/AVAnonymousUtils.h
--- JellyComic/Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ JellyComic/Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -82,10 +82,3 @@ strip_invalid_archs() {
   fi
 }
 
-
-if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${PODS_ROOT}/AVOSCloudDynamic/iOS/release-v3.1.6.3/Dynamic/AVOSCloud.framework"
-fi
-if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${PODS_ROOT}/AVOSCloudDynamic/iOS/release-v3.1.6.3/Dynamic/AVOSCloud.framework"
-fi
